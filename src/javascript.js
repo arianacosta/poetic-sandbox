53fcbc6e7decbd0a6477ca37b7f170c4fb83068a
Arian Acosta <13475001+arianacosta@users.noreply.github.com>
2019-09-06 18:28:22 -0400

    added some test cases

---
+++ src/javascript.js
@@ -0,0 +1,12 @@
+var item = abc; // no-var
+
+const item = new Object(); // no-new-object
+
+// object-shorthand
+const lukeSkywalker = 'Luke Skywalker';
+
+const obj = {
+  lukeSkywalker: lukeSkywalker,
+};
+
+const doubleQuotes = "Capt. Janeway${123}"; // quotes
