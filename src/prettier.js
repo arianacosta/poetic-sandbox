6df1b1045bb87e3471892031db965ee975f3a5b0
Arian Acosta <13475001+arianacosta@users.noreply.github.com>
2019-09-14 08:36:11 -0400

    added prettier example

---
+++ src/prettier.js
@@ -0,0 +1,8 @@
+const longLine = "This line is supposed" + "to be longer than" + "80 characters";
+
+const longestLine = "This line is supposed" + "to be even longer  than longer than" + "100 characters";
+
+const myObject = {
+    a: "this should single quotes",
+    b: "this should get a trailing comma"
+} // this should get a semi-colon
